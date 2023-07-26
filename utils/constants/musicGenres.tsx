4c3ec85f576c2f83ada495c27d76c89e879d3e9d
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-07-26 13:35:17 -0400

    Adding a list of genres for music

---
+++ utils/constants/musicGenres.tsx
@@ -0,0 +1,182 @@
+export const musicGenres = [
+    {
+        cover: "",
+        name: "A Capella",
+    },
+    {
+        cover: "",
+        name: "Acoustic",
+    },
+    {
+        cover: "",
+        name: "African",
+    },
+    {
+        cover: "",
+        name: "Arab",
+    }, 
+    {
+        cover: "",
+        name: "Alternative Rock",
+    },
+    {
+        cover: "",
+        name: "Bass Music",
+    },
+    {
+        cover: "",
+        name: "Beats",
+    },
+    {
+        cover: "",
+        name: "Bluegrass",
+    },
+    {
+        cover: "",
+        name: "Blues",
+    },
+    {
+        cover: "",
+        name: "Childrens",
+    },
+    {
+        cover: "",
+        name: "Country",
+    },
+    {
+        cover: "",
+        name: "Crunk",
+    },
+    {
+        cover: "",
+        name: "Dance",
+    },
+    {
+        cover: "",
+        name: "Dancehall",
+    },
+    {
+        cover: "",
+        name: "EDM",
+    },
+    {
+        cover: "",
+        name: "Electronic",
+    },
+    {
+        cover: "",
+        name: "Folk",
+    },
+    {
+        cover: "",
+        name: "Footwork",
+    },
+    {
+        cover: "",
+        name: "Freestyle",
+    },
+    {
+        cover: "",
+        name: "Funk",
+    },
+    {
+        cover: "",
+        name: "Gospel",
+    },
+    {
+        cover: "",
+        name: "Gospel Reggae",
+    },
+    {
+        cover: "",
+        name: "Grunge",
+    },
+    {
+        cover: "",
+        name: "Hard Rock",
+    },
+    {
+        cover: "",
+        name: "Hardcore",
+    },
+    {
+        cover: "",
+        name: "Harp",
+    },
+    {
+        cover: "",
+        name: "Hawaiian",
+    },
+    {
+        cover: "",
+        name: "Hip Hop",
+    },
+    {
+        cover: "",
+        name: "Indie",
+    },
+    {
+        cover: "",
+        name: "Israeli",
+    },
+    {
+        cover: "",
+        name: "Irish",
+    },
+    {
+        cover: "",
+        name: "Jazz",
+    },
+    {
+        cover: "",
+        name: "Jerk",
+    },
+    {
+        cover: "",
+        name: "Latin",
+    },
+    {
+        cover: "",
+        name: "Metal",
+    },
+    {
+        cover: "",
+        name: "Pop",
+    },
+    {
+        cover: "",
+        name: "Punk"
+    },
+    {
+        cover: "",
+        name: "R&B",
+    },
+    {
+        cover: "",
+        name: "Rap",
+    },
+    {
+        cover: "",
+        name: "Reggae",
+    },
+    {
+        cover: "",
+        name: "Rock",
+    },
+    {
+        cover: "",
+        name: "Romantic",
+    },
+    {
+        cover: "",
+        name: "Salsa",
+    },
+    {
+        cover: "",
+        name: "Underground",   
+    },
+    {
+        cover: "",
+        name: "Zydeco",
+    },
+];
