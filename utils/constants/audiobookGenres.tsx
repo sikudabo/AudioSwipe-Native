3ba2dc49c3ae475fa10c4bbf40ecb95e1fa7604c
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-07-26 17:12:41 -0400

    Adding the photos for audiobook genres

--- utils/constants/audiobookGenres.tsx
+++ utils/constants/audiobookGenres.tsx
@@ -1,62 +1,73 @@
+const Adventure = require('../../assets/app-media/adventure.jpeg');
+const Biblical = require('../../assets/app-media/sermons.jpeg');
+const Biography = require('../../assets/app-media/biography.jpeg');
+const Childrens = require('../../assets/app-media/childrens.jpeg');
+const Churchleadership = require('../../assets/app-media/church-leadership.jpeg');
+const Cooking = require('../../assets/app-media/cooking.jpeg');
+const Comedy = require('../../assets/app-media/comedy.jpeg');
+const Counseling = require('../../assets/app-media/counseling.jpeg');
+const Devotionals = require('../../assets/app-media/devotional.jpeg');
+const Education = require('../../assets/app-media/education.jpeg');
+const Evangelism = require('../../assets/app-media/evangelism.jpeg');
+const Fiction = require('../../assets/app-media/fiction.jpeg');
+const MentalHealth = require('../../assets/app-media/mental-health.jpeg');
+const Relationships = require('../../assets/app-media/relationships.jpeg');
+
 export const audiobookGenres = [
     {
-        cover: "",
+        cover: Adventure,
         name: "Adventure",
     },
     {
-        cover: "",
+        cover: Biblical,
         name: "Biblical",
     },
     {
-        cover: "",
+        cover: Biography,
         name: "Biography",
     },
     {
-        cover: "",
+        cover: Childrens,
         name: "Childrens",
     },
     {
-        cover: "",
+        cover: Churchleadership,
         name: "Church Leadership",
     },
     {
-        cover: "",
+        cover: Cooking,
         name: "Cooking",
     },
     {
-        cover: "",
+        cover: Comedy,
         name: "Comedy",
     },
     {
-        cover: "",
+        cover: Counseling,
         name: "Counseling",
     },
     {
-        cover: "",
+        cover: Devotionals,
         name: "Devotionals",
     },
     {
-        cover: "",
+        cover: Education,
         name: "Education",
     },
     {
-        cover: "",
+        cover: Evangelism,
         name: "Evangelism",
     },
     {
-        cover: "",
+        cover: Fiction,
         name: "Fiction",
     },
     {
-        cover: "",
+        cover: MentalHealth,
         name: "Mental Health",
     },
     {
-        cover: "",
+        cover: Relationships,
         name: "Relationships",
-    },
-    {
-        cover: "",
-        name: "Romance",
     },
 ];
