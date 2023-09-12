9442c952347b1075295daf74566ec079fb56c0bc
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-09-12 02:35:40 -0400

    Removing unused vars in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,15 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ActivityIndicator, MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import {  MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, View } from 'react-native';
 import { useFonts, VarelaRound_400Regular } from '@expo-google-fonts/varela-round';
-import { useShowLoader } from './hooks';
 import { AudioSwipeDialog, AudioSwipeNavigationTabs } from './components';
 import { colors } from './components/colors';
 import * as SplashScreen from 'expo-splash-screen';
 import { FanLoginPage, FanSignUpPage } from './pages';
 import GlobalContextProciders from './GlobalContextProviders';
-import DiscoverMusicPlayer from './pages/FanDiscoverPage/Discover/DiscoverMusicPlayer';
 import 'react-native-gesture-handler';
 
 
@@ -21,7 +19,6 @@ const queryClient = new QueryClient();
 
 type AppDisplayLayerProps = {
   fontsLoaded: boolean;
-  isLoading: boolean;
 }
 
 const theme = {
@@ -37,7 +34,7 @@ export default function App() {
 }
 
 
-function App_DisplayLayer({ fontsLoaded, isLoading }: AppDisplayLayerProps) {
+function App_DisplayLayer({ fontsLoaded }: AppDisplayLayerProps) {
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -109,10 +106,8 @@ function useDataLayer() {
   const [fontsLoaded] = useFonts({
     VarelaRound_400Regular,
   });
-  const { isLoading } = useShowLoader();
 
   return {
     fontsLoaded,
-    isLoading,
   }
 }
